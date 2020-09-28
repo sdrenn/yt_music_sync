6855250a69202fdd1d9cbafd8600a996ae857da1
Артемьев Михаил <m.artemiev@fund-f.ru>
2020-09-28 15:10:47 +0300

    on windows get ffmpeg from folder

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ url_list = ['https://www.youtube.com/watch?v=Sw2J4plpoYU']
 
 ydl_opts = {
     'format': 'bestaudio/best',
-    'ffmpeg_location': os.path.join(os.getcwd(), 'bin/codecs'),
     'outtmpl': os.path.join(os.getcwd(), 'music/%(title)s.%(ext)s'),
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
@@ -13,6 +12,10 @@ ydl_opts = {
         'preferredquality': '256',
     }],
 }
+
+# On Windows get ffmpeg from folder
+if os.name == 'nt':
+    ydl_opts['ffmpeg_location'] = os.path.join(os.getcwd(), 'bin/codecs')
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download(url_list)
